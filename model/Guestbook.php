a2d6c7113958ac0491bc4a9b7632246213ae111a
Amanda Williams <amandacwilliams97@gmail.com>
2019-05-10 14:22:47 -0400

    add model and tdd folders. Added Guestbook and GuestbookTest classes.

---
+++ model/Guestbook.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mandy
+ * Date: 5/10/2019
+ * Time: 11:17 AM
+ */
+
+class Guestbook
+{
+
+}
---
+++ tdd/GuestbookTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mandy
+ * Date: 5/10/2019
+ * Time: 11:19 AM
+ */
+
+use PHPUnit\Framework\TestCase as TestCase;
+
+class GuestbookTest extends TestCase
+{
+
+}
