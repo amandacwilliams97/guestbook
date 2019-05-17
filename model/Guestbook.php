be8671347f95e97e75f498dc46d036bd573d7477
Amanda Williams <amandacwilliams97@gmail.com>
2019-05-17 14:31:07 -0400

    added deleteAll() and tested

--- model/Guestbook.php
+++ model/Guestbook.php
@@ -8,5 +8,20 @@
 
 class Guestbook
 {
+    private $_entries = array(
+        array('name'=>'Celine',
+            'message'=>'Hi'),
+        array('name'=>'Ean',
+            'message'=>'Hello')
+    );
 
+    function viewAll()
+    {
+        return $this->_entries;
+    }
+
+    function deleteAll()
+    {
+        $this->_entries=array();
+    }
 }
--- tdd/GuestbookTest.php
+++ tdd/GuestbookTest.php
@@ -10,5 +10,40 @@ use PHPUnit\Framework\TestCase as TestCase;
 
 class GuestbookTest extends TestCase
 {
+    private$guestbook;
 
+    public function setUp()
+    {
+        $this->guestbook = new Guestbook();
+    }
+
+    //Test to ensure that the guestbook
+    //contains at least one entry
+    public function  testCountGuestbookEntries()
+    {
+        $entries = $this->guestbook->viewAll();
+        $this->assertInternalType('array', $entries);
+        $this->assertTrue(count($entries) >0);
+    }
+
+    public function tearDown()
+    {
+        unset($this->guestbook);
+    }
+
+    //Test to ensure that the deleteAll() function
+    //returns true and creates an empty array
+    public function testDeleteAll()
+    {
+        $return = $this->guestbook->deleteAll();
+        $entries = $this->guestbook->viewAll();
+        $this->assertInternalType('array', $entries);
+        $this->assertTrue(count($entries) == 0);
+    }
 }
+
+//this is the command you type to run this test program
+//...]$ vendor/bin/phpunit --bootstrap vendor/autoload.php tdd/GuestbookTest
+//call phpunit
+//bootstrap ... autoload runs auto load before testing
+//tdd/GuestbookTest instructs to execute tests of this class
